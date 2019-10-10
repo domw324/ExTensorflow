3db28dea65c9aadec8692537355611cad8859429
unknown <domw324@gmail.com>
2019-10-10 22:49:47 +0900

    학습 조기 종료 예제 준비

---
+++ ExKeras/02.Deep_Learning.py
@@ -0,0 +1,23 @@
+from keras.utils import np_utils
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+np.random.seed(3)
+
+# 1. 데이터셋 준비하기
+
+# 1-1. 훈련셋과 시험셋 로딩
+
+# 1-2. 훈련셋과 검증셋 분리
+
+# 1-3. 훈련셋, 검증셋 고르기
+
+# 1-4. 라벨링 전환
+
+# 2. 모델 구성하기
+
+# 3. 모델 엮기
+
+# 4. 모델 학습시키기
